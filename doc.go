5f366a77fd1fd7ccda027729ca3e7a56e94caf15
Jamie Stephens <james_stephens@cable.comcast.com>
2019-01-15 10:04:42 -0600

    Some more package summary docs

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package sheens provides specification-driven message-processing machinery.
+//
+// The core code is in package 'core', and some command-line tools are in `cmd`.
+//
+// See https://github.com/Comcast/sheens/blob/master/README.md for more.
+package sheens
