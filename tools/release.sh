b9893fbd45734884ea76d66c88faeddfddb08710
Jamie Stephens <james_stephens@cable.comcast.com>
2019-04-15 17:20:48 -0500

    Put release programs in bin/

--- tools/release.sh
+++ tools/release.sh
@@ -2,7 +2,9 @@
 
 # Build a release
 #
-# Usage: VERSION=0.1.2 ./rel.sh [GOOS] [GOARCH]
+# Usage: VERSION=0.1.2 ./release.sh [GOOS] [GOARCH]
+#
+# ToDo: Use Docker.
 
 set -e
 
@@ -16,19 +18,18 @@ echo $VERSION
 REL=sheens-$GOOS-$GOARCH-$VERSION
 TARGET=`pwd`/rel/$REL
 rm -rf $TARGET
-mkdir -p $TARGET
+mkdir -p $TARGET/bin
 for D in cmd/mexpect cmd/patmatch cmd/spectool cmd/mqclient cmd/siomq cmd/siostd; do
     echo $D
-    # -ldflags "-X main.GitCommit=$$(git rev-list -1 HEAD) -X main.Version=$${VERSION:-NA}"
-    (cd $D && go build -o $TARGET/$(basename $D) -ldflags="-s -w -X main.GitCommit=$(git rev-list -1 HEAD) -X main.Version=${VERSION:-NA}" )
+    (cd $D && go build -o $TARGET/bin/$(basename $D) -ldflags="-s -w -X main.GitCommit=$(git rev-list -1 HEAD) -X main.Version=${VERSION:-NA}" )
 done
 
 go get -u github.com/bronze1man/yaml2json
 
-(cd $GOPATH/src/github.com/bronze1man/yaml2json && go build -o $TARGET/yaml2json)
+(cd $GOPATH/src/github.com/bronze1man/yaml2json && go build -o $TARGET/bin/yaml2json)
 
 if [ "$GOOS" = "linux" -a "$GOARCH" = "amd64" ]; then
-    (cd $TARGET && /usr/bin/upx *)
+    (cd $TARGET/bin && /usr/bin/upx *)
 fi
 
 mkdir $TARGET/js
@@ -39,6 +40,7 @@ cp LICENSE $TARGET/LICENSE.txt
 
 echo "sheens $GOOS $GOARCH $VERSION $(git rev-list -1 HEAD)" > $TARGET/VERSION.txt
 date --rfc-3339=ns -u >> $TARGET/VERSION.txt
+echo "https://github.com/Comcast/sheens" > $TARGET/VERSION.txt
 
 echo "https://github.com/Comcast/sheens" > $TARGET/README.txt
 
