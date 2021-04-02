fe9225d9131a92ff6e8f5e98ec34ada8bbb6a1e2
Andrew Gerhold <andrew.broadway@gmail.com>
2021-04-02 02:18:20 +0000

    removed dot import

--- cmd/mcrew/timers_glue.go
+++ cmd/mcrew/timers_glue.go
@@ -17,7 +17,7 @@ import (
 	"fmt"
 	"time"
 
-	. "github.com/Comcast/sheens/util/testutil"
+	testutils "github.com/Comcast/sheens/util/testutil"
 )
 
 // toTimers is a tedious method that attempts to interpret msg as
@@ -25,19 +25,19 @@ import (
 func (s *Service) toTimers(ctx context.Context, msg interface{}) error {
 	m, is := msg.(map[string]interface{})
 	if !is {
-		return fmt.Errorf("%s (%T) isn't a %T", JS(msg), msg, m)
+		return fmt.Errorf("%s (%T) isn't a %T", testutils.JS(msg), msg, m)
 	}
 
 	if v, have := m["makeTimer"]; have {
 
 		if m, is = v.(map[string]interface{}); !is {
-			return fmt.Errorf("makeTimer: %s (%T) isn't a %T", JS(msg), msg, m)
+			return fmt.Errorf("makeTimer: %s (%T) isn't a %T", testutils.JS(msg), msg, m)
 		}
 
 		var id string
 		if x, have := m["id"]; have {
 			if id, is = x.(string); !is {
-				return fmt.Errorf("id %s (%T) isn't a %T", JS(x), x, id)
+				return fmt.Errorf("id %s (%T) isn't a %T", testutils.JS(x), x, id)
 			}
 		}
 
@@ -46,7 +46,7 @@ func (s *Service) toTimers(ctx context.Context, msg interface{}) error {
 		if x, have := m["in"]; have {
 			str, is := x.(string)
 			if !is {
-				return fmt.Errorf("'in' %s (%T) isn't a %T", JS(x), x, id)
+				return fmt.Errorf("'in' %s (%T) isn't a %T", testutils.JS(x), x, id)
 			}
 			if d, err = time.ParseDuration(str); err != nil {
 				return fmt.Errorf("bad duration '%s': %s", str, err)
@@ -54,7 +54,7 @@ func (s *Service) toTimers(ctx context.Context, msg interface{}) error {
 		} else if x, have := m["at"]; have {
 			str, is := x.(string)
 			if !is {
-				return fmt.Errorf("'in' %s (%T) isn't a %T", JS(x), x, id)
+				return fmt.Errorf("'in' %s (%T) isn't a %T", testutils.JS(x), x, id)
 			}
 			var t time.Time
 			t, err = time.Parse(time.RFC3339, str)
@@ -63,12 +63,12 @@ func (s *Service) toTimers(ctx context.Context, msg interface{}) error {
 			}
 			d = t.Sub(time.Now())
 		} else {
-			return fmt.Errorf("no 'at' or 'in' in %s", JS(m))
+			return fmt.Errorf("no 'at' or 'in' in %s", testutils.JS(m))
 		}
 
 		msg, have := m["message"]
 		if !have {
-			return fmt.Errorf("no 'message' in %s", JS(m))
+			return fmt.Errorf("no 'message' in %s", testutils.JS(m))
 		}
 
 		if err = s.timers.Add(ctx, id, msg, d); err != nil {
@@ -77,13 +77,13 @@ func (s *Service) toTimers(ctx context.Context, msg interface{}) error {
 	} else if x, have := m["deleteTimer"]; have {
 		id, is := x.(string)
 		if !is {
-			return fmt.Errorf("id %s (%T) isn't a %T", JS(x), x, id)
+			return fmt.Errorf("id %s (%T) isn't a %T", testutils.JS(x), x, id)
 		}
 		if err := s.timers.Rem(ctx, id); err != nil {
 			return fmt.Errorf("error for deleteTimer %s: %s", id, err)
 		}
 	} else {
-		return fmt.Errorf("no 'makeTimer' or 'deleteTimer' in %s", JS(msg))
+		return fmt.Errorf("no 'makeTimer' or 'deleteTimer' in %s", testutils.JS(msg))
 	}
 
 	return nil
